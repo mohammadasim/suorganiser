adc2752b44843998f79120060e7fa2c9d2ab126f
mohammad.ayub <asimayub81@gmail.com>
2020-04-10 15:55:27 +0100

    added new apps and setup docker-compose for the project

--- suorganizer/suorganizer/settings/base.py
+++ suorganizer/suorganizer/settings/base.py
@@ -11,21 +11,30 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """
+    Get the environment variable or return exception
+    """
+    try:
+        return os.environ.get(var_name)
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd^rx*6=hi)_k-ci=o-v@(#zt(npauwbc4@zv!h$t0j0l^sq&*b'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'blogs.apps.BlogsConfig',
+    'organizers.apps.OrganizersConfig',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'suorganizer.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -79,7 +90,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -112,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
---
+++ suorganizer/suorganizer/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('PGSQL_DB_NAME'),
+        'USER': get_env_variable('PGSQL_DB_USER'),
+        'PASSWORD': get_env_variable('PGSQL_DB_PASW'),
+        'HOST': get_env_variable('PGSQL_DB_HOST'),
+        'PORT': get_env_variable('PGSQL_DB_PORT')
+    }
+}
