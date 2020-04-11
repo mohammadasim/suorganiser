7d0ede874f7ae0c70c0e2a5c701070784c301f93
mohammad.ayub <asimayub81@gmail.com>
2020-04-11 18:26:32 +0100

    added models

--- suorganizer/blogs/models.py
+++ suorganizer/blogs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from organizers.models import Tag, Startup
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(
+        max_length=63,
+        help_text='A label for URL config',
+        unique_for_month='pub_date'
+    )
+    text = models.TextField()
+    pub_date = models.DateField(
+        'Date Published',
+        auto_now_add=True
+    )
+    tags = models.ManyToManyField(Tag,
+                                  related_name='blog_posts')
+    startups = models.ManyToManyField(Startup,
+                                      related_name='blog_posts')
--- suorganizer/organizers/models.py
+++ suorganizer/organizers/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31,
+                            unique=True)
+    slug = models.SlugField(unique=True,
+                            max_length=31,
+                            help_text='A label for URL Config')
+
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31,
+                            db_index=True)
+    slug = models.SlugField(max_length=31,
+                            unique=True,
+                            help_text='A label for URL Config')
+    description = models.TextField()
+    founded_date = models.DateField('Date Founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('Date Published')
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup)
+
