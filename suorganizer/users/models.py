c884668ef3f0bc1caf788b6dab83e9bba69d6e0f
Mohammad Ayub <mohammad.ayub@imagination.com>
2020-08-13 07:35:46 +0100

    added profile model to user

--- suorganizer/users/models.py
+++ suorganizer/users/models.py
@@ -1,1 +1,22 @@
-# Create your models here.
+from django.conf import settings
+from django.db import models
+
+
+class Profile(models.Model):
+    """
+    A model representing a user profile
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True
+    )
+    slug = models.SlugField(
+        max_length=30,
+        unique=True
+    )
+    about = models.TextField()
+
+    def __str__(self):
+        return self.user.get_username()
+
