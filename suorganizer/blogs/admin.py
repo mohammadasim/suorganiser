8989ecd94604df428172cbbcdac1963cde2e4c73
Mohammad Ayub <asimayub81@gmail.com>
2020-08-25 08:43:44 +0100

    configured user display page in the admin app

--- suorganizer/blogs/admin.py
+++ suorganizer/blogs/admin.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import Post
 
@@ -10,16 +13,38 @@ class PostAdmin(admin.ModelAdmin):
     to modify the behaviour of Post model
     in the admin app.
     """
+
     # Aggregate functions allow us to perform
     # calculations directly in the database
     # As calling the database is an expensive
     # operation
-
+    def get_queryset(self, request):
+        """
+        overriding the default method
+        to perform tag count at
+        database level
+        Also allowing only users with
+        permissions to see future
+        publishing posts.
+        :param request:
+        :return:
+        """
+        queryset = super().get_queryset(request)
+        if not request.user.has_perms(
+                'view_future_post'
+        ):
+            queryset = queryset.filter(
+                pub_date__lte=datetime.now()
+            )
+        return queryset.annotate(
+            tag_number=Count('tags')
+        )
 
     def tag_count(self, post):
-        return post.tags.count()
+        return post.tag_number
 
     tag_count.short_description = 'Number of Tags'
+    tag_count.admin_order_field = 'tag_number'
 
     list_display = ('title', 'pub_date', 'tag_count')
     date_hierarchy = 'pub_date'
@@ -46,5 +71,6 @@ class PostAdmin(admin.ModelAdmin):
     )
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ('startups', 'tags')
+
 
 
--- suorganizer/users/admin.py
+++ suorganizer/users/admin.py
@@ -2,11 +2,59 @@ from django.contrib import admin
 from .models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """
     Custom user admin for user model
     """
-    pass
 
+    def get_date_joined(self, user):
+        """
+        Method to get the joined date
+        of the user from user profile,
+        user the relationship between
+        user and profile models.
+        :param user:
+        :return:
+        """
+        return user.profile.joined
 
-admin.site.register(User, UserAdmin)
+    get_date_joined.short_description = 'Joined'
+    get_date_joined.admin_order_field = 'profile__joined'
+
+    def get_name(self, user):
+        """
+        Method to get the name
+        of the user from its
+        profile using the
+        relationship between
+        the two modules
+        :param user:
+        :return:
+        """
+        return user.profile.name
+
+    # This sets the column name when displaying data
+    get_name.short_description = 'Name'
+    get_name.admin_order_field = 'profile__name'
+    # list view
+    list_display = (
+        'get_name',
+        'email',
+        'get_date_joined',
+        'is_staff',
+        'is_superuser'
+    )
+    list_filter = (
+        'is_staff',
+        'is_superuser',
+        'profile__joined'
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    # The values in the below tuple
+    # will become links, linking to edit
+    # page for the instance of the user
+    # These values come form list_display
+
+    list_display_links = ('get_name', 'email')
