5c63bca9b44c4f757de950c6e1372419d8a3e8eb
mohammad.ayub <asimayub81@gmail.com>
2020-09-19 09:15:21 +0100

    added another custom tag

--- suorganizer/blogs/templatetags/blog_latest.py
+++ suorganizer/blogs/templatetags/blog_latest.py
@@ -79,6 +79,55 @@ def do_latest_posts(parser, token):
     return LatestPostsNode(number_of_posts)
 
 
+@register.tag(name='get_me_latest_posts')
+def do_latest_posts_asvar(parser, token):
+    """
+    Function to return the latest posts
+    with the flexibility to let user
+    set the variable in template
+    Using python3 extended unpacking
+    feature, we grab the name of the tag
+    and then any other arguments to the
+    token list.
+    The use of as must be the last thing
+    to occour.
+    This function is implemented as an
+    example, the function above can be
+    modified to make it flexible, but
+    the idea is to have both as an
+    example.
+    """
+    asvar = None
+    tag_name, *tokens = (
+        token.split_contents()
+    )
+    if len(tokens) > 2 and tokens[-2] == 'as':
+        # We will unpack the tuple now
+        *tokens, _, asvar = tokens
+    else:
+        raise TemplateSyntaxError(
+            f'{tag_name} expects three arguments '
+            f'number of posts, as <variable_name> '
+            f'The correct format was not provided.'
+        )
+        # We then check to ensure that
+    # only 1 item is now in *tokens
+    if len(tokens) != 1:
+        raise TemplateSyntaxError(
+            f'{tag_name} takes 1 argument: '
+            f'the number of posts.'
+        )
+    try:
+        number_of_posts = int(tokens.pop())
+    except ValueError:
+        raise TemplateSyntaxError(
+            f'{tag_name} expects argument to '
+            f'be an integer.'
+        )
+    return LatestPostsNodeAsVariable(number_of_posts, asvar)
+
+
+
 class LatestPostNode(Node):
     """
     A sub class of Node returning
@@ -118,4 +167,29 @@ class LatestPostsNode(Node):
         """
         context['latest_post_list'] = \
             Post.objects.published()[:self.num]
+        return str()
+
+
+class LatestPostsNodeAsVariable(Node):
+    """
+    Node subclass returning a list
+    of posts. The name of the list
+    can be setup at the template
+    level or will default  to
+    custom_post_list
+    """
+
+    def __init__(self, number_of_posts, asvar):
+        """
+        Init method for the class
+        """
+        self.num = number_of_posts
+        self.asvar = asvar
+
+    def render(self, context):
+        objs = Post.objects.published()[:self.num]
+        if self.asvar is None:
+            context['custom_post_list'] = objs
+        else:
+            context[self.asvar] = objs
         return str()
